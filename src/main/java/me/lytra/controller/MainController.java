e326ea5a9b4185e8cfc8be398ccc723b726a49db
Kalvin <2porkchops@gmail.com>
2014-12-10 18:20:29 -0600

    login page

--- src/main/java/me/lytra/controller/MainController.java
+++ src/main/java/me/lytra/controller/MainController.java
@@ -1,10 +1,9 @@
 package me.lytra.controller;
 
-import java.util.List;
-
 import javax.servlet.http.HttpSession;
 
 import me.lytra.domain.facebook.Feed;
+import me.lytra.domain.user.User;
 import me.lytra.persistence.service.BlogService;
 import me.lytra.persistence.service.GridFsService;
 import me.lytra.persistence.service.UserService;
@@ -14,8 +13,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 
@@ -41,6 +42,36 @@ public class MainController {
 		return "greeting";
 	}*/
 
+    @RequestMapping(value="/admin", method=RequestMethod.GET)
+	public ModelAndView handleLogin(HttpSession session) {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("login");
+		mav.addObject("user", new User());
+		return mav;	
+	}
+    
+    
+	@RequestMapping(value="/admin/login", method=RequestMethod.POST)
+	public String login(HttpSession session, @ModelAttribute User user) {
+		logger.info("User login attempt: {}", user);
+		User validUser = userService.login(user.getUsername(), user.getPassword());
+		if(validUser!=null){			
+			logger.info("User Authenticated: {}", validUser);
+			session.setAttribute("USER_OBJECT", validUser);
+			session.setAttribute("USER_ID", validUser.getId());
+			if(validUser.isAdmin()){
+				session.setAttribute("USER_ADMIN", validUser.isAdmin());
+				logger.info("User admin login successful: {}", session.getAttribute("USER_OBJECT").toString());
+			}
+			return "redirect:/admin/dashboard";
+		}
+		
+		else {
+			logger.warn("Failed login attempted with username: {}", user.getUsername());
+			return "noaccess";			
+		}
+	}	
+	
     @RequestMapping(value="/indexold", method=RequestMethod.GET)
 	public ModelAndView handleIndex(HttpSession session) {
     	logger.info("Index accessed");
